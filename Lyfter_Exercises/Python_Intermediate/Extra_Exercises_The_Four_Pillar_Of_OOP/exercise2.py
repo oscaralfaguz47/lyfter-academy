2f1c7a93f5db96a99bb8b183c1e897b241d41db2
Oscar Alfaro <oscar.alfaguz47@gmail.com>
2026-08-26 17:21:22 -0600

    Extra exercises about the four pillars of OOP

---
+++ Lyfter_Exercises/Python_Intermediate/Extra_Exercises_The_Four_Pillar_Of_OOP/exercise2.py
@@ -0,0 +1,38 @@
+from abc import ABC, abstractmethod
+
+class User(ABC):
+    def __init__(self, user_name, permissions):
+        self.user_name = user_name
+        self.permissions = permissions
+
+    @abstractmethod
+    def get_role(self):
+        pass
+
+    def has_permission(self, permission):
+        return permission in self.permissions
+
+
+class AdminUser(User):
+    def __init__(self, user_name):
+        super().__init__(user_name, [])
+
+    def get_role(self):
+        return "Admin"
+
+    def has_permission(self, permission):
+        return True
+
+class RegularUser(User):
+    def __init__(self, user_name):
+        super().__init__(user_name,  ["read"])
+
+    def get_role(self):
+        return "Regular"
+
+
+user_admin = AdminUser("Oscar")
+user_regular = RegularUser("Emilio")
+
+print(user_admin.has_permission("delete"))
+print(user_regular.has_permission("delete"))
