abb7f8fc0d1758283cfcaace520c3dcbe46d3001
Oscar Alfaro <oscar.alfaguz47@gmail.com>
2026-08-26 22:40:39 -0600

    Fixing exercises 1 and 2

--- Lyfter_Exercises/Python_Basics/Exceptions/greeting_with_exceptions.py
+++ Lyfter_Exercises/Python_Basics/Exceptions/greeting_with_exceptions.py
@@ -1,21 +1,26 @@
 print("----- Greeting -----")
 
-try:
+def enter_name():
     user_name = input("Enter your name: ")
     if user_name.isdigit():
         raise ValueError("The name cannot be numeric")
-    user_age = input("Enter your age: ")
-    if not user_age.isdigit():
+    return user_name
+
+def enter_age():
+    try:
+        return int(input("Enter your age: "))
+    except ValueError:
         raise ValueError("Not valid number, enter numeric only")
+
+
+def print_result(user_name, user_age):
     print(f"Hello {user_name}, your age is {user_age}")
+
+try:
+    user_name = enter_name()
+    user_age = enter_age()
+    print_result(user_name, user_age)
 except ValueError as e:
-    print(f"Error: {e}")
+    print({e})
 
 
-
-
-
-
-
-# Ask the name to the user, if the name is numeric do raise ValueError("The name cannot be numeric")
-# Ask the age to the user, if it's not a valid number, capture the ValueError and show the validation message
